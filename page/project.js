d67868477529dc0a6ef89531d878f62c03d3e535
adin979 <adinmaldini91@gmail.com>
2025-09-13 16:09:05 +0700

    modification project

--- page/project.js
+++ page/project.js
@@ -1,15 +1,21 @@
 import projects from '../privat/projects.js';
 export default function project(){
 const app = document.getElementById('app');
-const div = document.createElement('div');
+const div = '';
 projects.map(p=>{
-    let title = document.createElement('p');
-    let path = document.createElement('a');
-    title.textContent=p.title;
-    path.href=p.path;
-    path.textContent='Download';
-    div.appendChild(title);
-    div.appendChild(path);
+    // let title = document.createElement('p');
+    // let path = document.createElement('a');
+    // title.textContent=p.title;
+    // path.href=p.path;
+    // path.textContent='Download';
+    // div.appendChild(title);
+    // div.appendChild(path);
+    div += `
+    <div>
+        <p>${p.title}</p>
+        <a hreff="${path}">Download</a>
+    </div>
+    `
 })
 app.innerHTML = div;
 // app.appendChild(div);
