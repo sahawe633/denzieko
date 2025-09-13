f6a3f89e5be13d62a12562ca50a1470c42d58fb2
adin979 <adinmaldini91@gmail.com>
2025-09-13 13:42:06 +0700

    modification home

--- page/home.js
+++ page/home.js
@@ -1,36 +1,29 @@
 import carousel from '../style/carousel.js'
 function home(){
 	document.getElementById('app').innerHTML=
-	`<section>
-  <div class="jumbotron ">
-  	<div class="tagline">
-  		<h1>
-Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Minima dolor similique ab?</h1>
-  	</div>
-  </div>
-</section>
-<section>
-   <div class="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item left"></div>
-      <div class="carousel-item mid"></div>
-      <div class="carousel-item right"></div>
-    </div>
-  </div>
-</section>
-<section class=" home-about">
-  <div class="row ">
-    <div class="col">
-  		<img src="image/rias-makeup-7.png" alt="photo">
-    </div>
-    <div class="col ">
-    	<p>
-    	itaque fuga dicta autem quam rem nulla Similique, quis delectus voluptatibus aliquam consequatur ullam reiciendis, beatae accusantium est, dolorum repudiandae totam tenetur. Quo, autem? Totam, dolorem porro commodi architecto, itaque ex aut sequi! Eligendi ipsam eum accusantium nulla nostrum repellendus sapiente possimus odio incidunt! Laboriosam sint laudantium ducimus velit repudiandae. Error, itaque, soluta?
-      </p>
-    </div>
-  </div>
-</section>
-`
+	`<div class="container">	
+		<div class="content">
+			fauzi
+		</div>
+		<div class="pict">
+			<img src="./profil.jpg" alt="img">
+		</div>
+	</div>
+	<section>
+		<div class="wrapper">
+			<div class="wrap">
+				<h1>sedikit tentang saya</h1>
+				
+					<img src="./music.jpg" alt="img">
+					<p>
+						Saya senang mempelajari apapun, dan saya suka sekali dengan musik.
+						Banyak hal yang bisa saya pelajari dari kehidupan ini, mungkin pengalaman kamu juga bisa menjadi inspirasi dan pelajaran buat saya.
+						Kamu bisa mengenal lebih bsnyak tentang saya melalui website ini dengan membaca artikel tentang saya di <a href="/about">sini</a>
+						atau melaui akun sosmed saya.
+					</p>
+				</div>
+		</div>	
+	</section>`
 	document.querySelector('title').innerHTML='Home';
   carousel();
 }
