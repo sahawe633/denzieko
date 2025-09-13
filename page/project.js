b82809074486d0c8ebc704a38fbd1cd04273b1b8
adin979 <adinmaldini91@gmail.com>
2025-09-13 21:12:09 +0700

    fix: add flex-direction to project container for proper alignment

--- page/project.js
+++ page/project.js
@@ -2,7 +2,7 @@ import projects from '../privat/projects.js';
 const PATH = location.origin;
 const app = document.getElementById('app');
 const div = document.createElement('div');
-div.setAttribute('class','display_flex justify-content_center w_100vw h_100vh align-items_center')
+div.setAttribute('class','display_flex justify-content_center w_100vw h_100vh align-items_center flex-direction_column')
 projects.map(p=>{
     let title = document.createElement('p');
     let path = document.createElement('a');
@@ -13,6 +13,7 @@ projects.map(p=>{
     img.src=PATH+'/image/'+p.img;
     img.setAttribute('class','w_100px h_100px')
     div.appendChild(title);
+    div.appendChild(img);
     div.appendChild(path);
 })
 export default function project(){
