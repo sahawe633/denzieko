0acc2d54e65962600cf74d757cdc75010390154e
adin979 <adinmaldini91@gmail.com>
2025-09-13 17:33:56 +0700

    update project

--- page/project.js
+++ page/project.js
@@ -4,13 +4,23 @@ const div = document.createElement('div');
 div.setAttribute('class','display_flex justify-content_center w_100vw h_100vh align-items_center')
 projects.map(p=>{
     let title = document.createElement('p');
-    let path = document.createElement('a');
+    let path = document.createElement('p');
     title.textContent=p.title;
-    path.href='../file/'+p.path;
+    path.setAttribute('onclick',`download('../files/${p.path}',${p.path})`)
     path.textContent='Download';
     div.appendChild(title);
     div.appendChild(path);
 })
+const download=(url,filename)=>{
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click()
+
+    document.body.removeChild(a);
+}
 export default function project(){
     app.innerHTML = '';
     app.appendChild(div);
