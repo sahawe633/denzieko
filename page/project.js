0e76dc104f8dd644a710b1105bc38527f6a01909
adin979 <adinmaldini91@gmail.com>
2025-09-13 16:45:11 +0700

    modification project

--- page/project.js
+++ page/project.js
@@ -1,7 +1,7 @@
 import projects from '../privat/projects.js';
 const app = document.getElementById('app');
 const div = document.createElement('div');
-div.setAttribute('class','d_flex jc_center w_100vw h_100vh ai_center')
+div.setAttribute('class','display_flex justify-content_center w_100vw h_100vh align-items_center')
 projects.map(p=>{
     let title = document.createElement('p');
     let path = document.createElement('a');
