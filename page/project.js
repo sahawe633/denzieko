d71c07cf32a215bd236f16a414fb0cc05e106fcd
adin979 <adinmaldini91@gmail.com>
2025-09-13 15:43:02 +0700

    modification index

--- page/project.js
+++ page/project.js
@@ -10,9 +10,9 @@ projects.map(p=>{
     div.appendChild(title);
     div.appendChild(path);
 })
-const project=()=>{
+export default function project(){
     app.innerHTML = '';
     app.appendChild(div);
 }
 
-export default project();
+// export default project();
