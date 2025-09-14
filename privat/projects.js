a6baea4717e05c5010f16506e58530498fafae35
adin979 <adinmaldini91@gmail.com>
2025-09-14 20:31:02 +0700

    fix: update APK path for squi-mobile project

--- privat/projects.js
+++ privat/projects.js
@@ -3,7 +3,7 @@ let projects = [
         title:'squi-mobile',
         category:'game-mobile',
         id:'1',
-        path:'squi-v1.1.apk',
+        path:'squi-v1.2.apk',
         description:'game mobile',
         img:'squirel.png'
     }
